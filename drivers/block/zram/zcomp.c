412cb06195a9698706cfa2537f46f7e14400d092
kyzuna28 <dzakysptr28@gmail.com>
2026-06-13 15:05:43 +0700

    lzo-rle

--- drivers/block/zram/zcomp.c
+++ drivers/block/zram/zcomp.c
@@ -20,6 +20,7 @@
 
 static const char * const backends[] = {
 	"lzo",
+	"lzo-rle",
 #if IS_ENABLED(CONFIG_CRYPTO_LZ4)
 	"lz4",
 #endif
@@ -28,6 +29,9 @@ static const char * const backends[] = {
 #endif
 #if IS_ENABLED(CONFIG_CRYPTO_LZ4HC)
 	"lz4hc",
+#endif
+#if IS_ENABLED(CONFIG_CRYPTO_LZ4KD)
+	"lz4kd",
 #endif
 #if IS_ENABLED(CONFIG_CRYPTO_842)
 	"842",
